ba810d965f28dfdb40fbe147068d482971bfbf2f
jward6301 <jward6301@gmail.com>
2023-08-18 21:36:10 +0000

    I have made all changes to the py file and read me file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # health-analytics
-This is an assignment from HHA 506 as a primer for the HHA 504 &amp; 507 classes.
+This is an assignment from HHA 506 as a primer for the Fall HHA 504 &amp; 507 classes.
+
+
+## Summary of Variables
+The number variables are ages and weights for both patients. 
+The string variable is the patient's names.
+The list is the Chief complaints.
+The dictionary is past medical history that includes chief complaints, medical conditions, curent medications and past medications
+
+## Explanation of the functions
+I created two functions, one focusing on blood sugar and the other on BPM. Each function's outcome is dependent on two variables. For the blood sugar function, it is dependent on the blood sugar level and medical conditions, under past medical history. If the blood sugar is above 100 and the medical conditions states a history of hyperglycemia, it will return monitor patient and provide insulin. If neither conditions are met or only one is met, it will return patient is okay to go home. for the BPM function, it is dependent on the BPM and medical conditions, under past medical history. If the BPM is above 120 and the medical conditions states a history of hypertension, it will return monitor patient and provide beta-blockers. If neither conditions are met or only one is met, it will return patient is okay to go home.
+
+## Expected output based on example data
+The expected output is based off of the BPM function for both patients. For Pateint A, Frances, the expected output is monitor patient and provide beta-blockers. For Patient B, Hope, the expected outcome is patient is okay to go home. The output will also mention who each pateint is and what their BPM result is. 
--- health_analysis.py
+++ health_analysis.py
@@ -8,10 +8,11 @@ PatientA = {
     "Age": 23,
     "Sex": "F",
     "Weight": 145,
-    "ChiefComplaint": "headaches",
+    "ChiefComplaint": ["headaches", "tiredess"],
     "BPM": 135,
     "Bloodsugar": 75,
     "PastMedicalHistory": {
+        "ChiefComplaint": ["headaches", "tiredess"],
         "MedicalConditions": ["hypertension", "allergies", "acute eczema"],
         "CurrentMedications": ["flonase"],
         "PastMedications": ["claritin", "corticosteroids"]
@@ -22,10 +23,11 @@ PatientB = {
     "Age": 42,
     "Sex": "F",
     "Weight": 175,
-    "ChiefComplaint": "tiredness",
+    "ChiefComplaint": ["tiredness", "nasuea"],
     "BPM": 94,
     "Bloodsugar": 210,
     "PastMedicalHistory": {
+        "ChiefComplaint": ["tiredness", "nasuea"],
         "MedicalConditions": ["hyperglycemia", "sleep apnea", "allergies"],
         "CurrentMedications": ["insulin", "claritin"],
         "PastMedications": ["modafinil"]
@@ -34,13 +36,13 @@ PatientB = {
 
 #functions
 
-def Bloodsugar_concern(BPM, Bloodsugar, MedicalConditions):
+def Bloodsugar_concern(Bloodsugar, MedicalConditions):
     if Bloodsugar > 100 and "hyperglycemia" in MedicalConditions:
         return "monitor patient and provide insulin"
     else:
         return "patient is okay to go home"
 
-def BPM_concern(BPM, Bloodsugar, MedicalConditions):
+def BPM_concern(BPM, MedicalConditions):
     if BPM > 120 and "hypertension" in MedicalConditions:
         return "monitor patient and provide beta-blockers"
     else:
@@ -59,9 +61,9 @@ for key, value in PatientB.items():
 
 #example run
 
-BPM_result = BPM_concern(PatientA["BPM"], PatientA["Bloodsugar"], PatientA["PastMedicalHistory"]["MedicalConditions"])
+BPM_result = BPM_concern(PatientA["BPM"], PatientA["PastMedicalHistory"]["MedicalConditions"])
 print("patient name:", PatientA["fullname"], "BPM_result:", BPM_result)
 
-BPM_result = BPM_concern(PatientB["BPM"], PatientB["Bloodsugar"], PatientB["PastMedicalHistory"]["MedicalConditions"])
+BPM_result = BPM_concern(PatientB["BPM"], PatientB["PastMedicalHistory"]["MedicalConditions"])
 print("patient name:", PatientB["fullname"], "BPM_result:", BPM_result)
 
